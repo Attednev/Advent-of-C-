a17cdaa22f7d57e6460375c933cd7fd7fbe38d9d
Damian Gasser <damian.gasser@gmail.com>
2023-12-04 11:23:10 +0100

    Cleanup

--- day03.cpp
+++ day03.cpp
@@ -1,6 +1,8 @@
 #include "day0.h"
 
-long long add_part_values(std::string& line, std::vector<std::pair<long long, char>>& positions, std::size_t number, std::size_t number_pos_size_t, std::vector<std::vector<long long>>& gear_ratio) {
+long long
+add_part_values(std::vector<std::pair<long long, char>> &positions, std::size_t number, std::size_t number_pos_size_t,
+                std::vector<std::vector<long long>> &gear_ratio) {
     long long number_length{static_cast<long long>(std::to_string(number).length())};
     long long number_position{static_cast<long long>(number_pos_size_t)};
     long long output{0};
@@ -46,7 +48,7 @@ void solve(std::vector<std::string> input) {
         for (std::size_t j = 1; (number = read_nth_number(input[i], j)) != 0; j++) {
             std::size_t number_position = read_nth_number(input[i], j, true);
             for (short k = 1; k <= 3; k++) {
-                long long parts_found = add_part_values(input[i], above, number, number_position, gear_ratio[i + k]);
+                long long parts_found = add_part_values(above, number, number_position, gear_ratio[i + k]);
                 if (parts_found != 0) {
                     total += parts_found;
                     break;
@@ -56,7 +58,7 @@ void solve(std::vector<std::string> input) {
     }
 
     std::size_t gear_sum{0};
-    for (std::vector<std::vector<long long>> v1 : gear_ratio) {
+    for (const std::vector<std::vector<long long>>& v1 : gear_ratio) {
         for (std::vector<long long> v : v1) {
             if (v.size() == 2) {
                 gear_sum += v[0] * v[1];
