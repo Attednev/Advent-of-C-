d99379b356776af688166d151c6a394595976daa
Damian Gasser <damian.gasser@gmail.com>
2023-12-06 23:40:34 +0100

    Update project structure

--- day17.cpp
+++
@@ -1,8 +0,0 @@
-#include "day0.h"
-
-void solve(std::vector<std::string> lines) {
-}
-
-int main() {
-    solve(read_input_file(17));
-}
---
+++ day21/day21.cpp
@@ -0,0 +1,8 @@
+#include "../day0.h"
+
+void solve(std::vector<std::string> lines) {
+}
+
+int main() {
+    solve(read_input_file("../day21/day21_input"));
+}
