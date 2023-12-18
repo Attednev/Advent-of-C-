ec8e907ee279e0dbacd1bba51e022f59bae1878d
Damian Gasser <damian.gasser@gmail.com>
2023-12-18 14:30:18 +0100

    Update day 10 solution

--- day10.cpp
+++ day10.cpp
@@ -1,78 +1,81 @@
 #include "day0.h"
+#include <chrono>
+#include <thread>
 
 enum direction { NORTH, EAST, SOUTH, WEST, NONE };
 
-struct position {
-    std::size_t i{range_index::npos}, j{range_index::npos};
-    direction d{NONE};
-    bool operator==(const position& other) const { return i == other.i && j == other.j; }
-    bool operator>=(const position& other) const { return i >= other.i || j >= other.j; }
-};
 
-#define pos_vec std::vector<position>
+char get_start_replacement(direction start_direction, direction last_direction) {
+    if (last_direction == start_direction && (last_direction == NORTH || last_direction == SOUTH)) // NN,SS: |
+        return '|';
+    if (last_direction == start_direction && (last_direction == EAST || last_direction == WEST)) // WW,OO: -
+        return '-';
+    if ((last_direction ^ start_direction) == 0x3 && (last_direction == NORTH || last_direction == EAST)) // NW,OS: 7
+        return '7';
+    if ((last_direction ^ start_direction) == 0x1 && (last_direction == NORTH || last_direction == WEST)) // NO,WS: F
+        return 'F';
+    if ((last_direction ^ start_direction) == 0x3 && (last_direction == SOUTH || last_direction == WEST)) // SO,WN: L
+        return 'L';
+    if ((last_direction ^ start_direction) == 0x1 && (last_direction == SOUTH || last_direction == EAST)) // SW,ON: J
+        return 'J';
+    return'S'; // No replacement found
+}
 
-bool get_maze_map(const std::vector<std::string>& lines, pos_vec& map_of_maze, const position start_pos, const position pos, const direction last_direction = NONE) {
-    if (last_direction != NONE && pos == start_pos)
-        return true;
-
+char get_maze_map(const auto& lines, auto& map_of_maze, const auto& start_pos, auto pos, direction last_direction = NONE) {
     std::vector<char> north{'|', 'F', '7', 'S'}, east{'-', 'J', '7', 'S'}, south{'|', 'J', 'L', 'S'}, west{'-', 'L', 'F', 'S'};
-    if (last_direction != SOUTH && pos.i > 0 && contains(north, lines[pos.i - 1][pos.j]) && contains(south, lines[pos.i][pos.j])
-        && get_maze_map(lines, map_of_maze, start_pos, {pos.i - 1, pos.j}, NORTH)) {
-        map_of_maze.emplace_back(pos.i, pos.j, NORTH);
-        return true;
+    direction start_direction{NONE};
+    while (last_direction == NONE || pos != start_pos) {
+        if (last_direction != SOUTH && pos.first > 0 && contains(north, lines[pos.first - 1][pos.second]) && contains(south, lines[pos.first][pos.second])) {
+            map_of_maze[pos.first--].emplace_back(pos.second);
+            last_direction = NORTH;
+        } else if (last_direction != WEST && pos.second < lines[0].length() - 1 && contains(east, lines[pos.first][pos.second + 1]) && contains(west, lines[pos.first][pos.second])) {
+            map_of_maze[pos.first].emplace_back(pos.second++);
+            last_direction = EAST;
+        } else if (last_direction != NORTH && pos.first < lines.size() - 1 && contains(south, lines[pos.first + 1][pos.second]) && contains(north, lines[pos.first][pos.second])) {
+            map_of_maze[pos.first++].emplace_back(pos.second);
+            last_direction = SOUTH;
+        } else if (last_direction != EAST && pos.second > 0 && contains(west, lines[pos.first][pos.second - 1]) && contains(east, lines[pos.first][pos.second])) {
+            map_of_maze[pos.first].emplace_back(pos.second--);
+            last_direction = WEST;
+        }
+        if (start_direction == NONE)
+            start_direction = last_direction;
     }
-    if (last_direction != WEST && pos.j < lines[0].length() - 1 && contains(east, lines[pos.i][pos.j + 1]) && contains(west, lines[pos.i][pos.j])
-        && get_maze_map(lines, map_of_maze, start_pos, {pos.i, pos.j + 1}, EAST)) {
-        map_of_maze.emplace_back(pos.i, pos.j, EAST);
-        return true;
-    }
-    if (last_direction != NORTH && pos.i < lines.size() - 1 && contains(south, lines[pos.i + 1][pos.j]) && contains(north, lines[pos.i][pos.j])
-        && get_maze_map(lines, map_of_maze, start_pos, {pos.i + 1, pos.j}, SOUTH)) {
-        map_of_maze.emplace_back(pos.i, pos.j, SOUTH);
-        return true;
-    }
-    if (last_direction != EAST && pos.j > 0 && contains(west, lines[pos.i][pos.j - 1]) && contains(east, lines[pos.i][pos.j])
-        && get_maze_map(lines, map_of_maze, start_pos, {pos.i, pos.j - 1}, WEST)) {
-        map_of_maze.emplace_back(pos.i, pos.j, WEST);
-        return true;
-    }
-
-    return false;
+    return get_start_replacement(start_direction, last_direction);
 }
 
-void fill_enclosed_tiles(pos_vec& map_of_maze, std::vector<short>& visited, position pos, position boundaries) {
-    if (pos >= boundaries || contains(map_of_maze, pos) || visited[pos.i * boundaries.j + pos.j] > 0)
-        return;
-    visited[pos.i * boundaries.j + pos.j]++;
-    fill_enclosed_tiles(map_of_maze, visited, {pos.i, pos.j + 1}, boundaries);
-    fill_enclosed_tiles(map_of_maze, visited, {pos.i + 1, pos.j}, boundaries);
-}
-
-std::size_t get_number_of_enclosed_tiles(const std::vector<std::string>& lines, position start_pos) {
-    direction last_direction{NONE};
-    pos_vec map_of_maze{}, enclosed_nodes{};
-    get_maze_map(lines, map_of_maze, start_pos, start_pos);
-    std::vector<short> visited(lines.size() * lines[0].length(), 0);
-    for (auto& [i, j, d] : map_of_maze) {
-        position neighbour{d == NORTH || d == SOUTH ? i : i + 2 - d, d == EAST || d == WEST ? j : j + 1 - d};
-        //position neighbour{d == NORTH || d == SOUTH ? i : i - 2 + d, d == EAST || d == WEST ? j : j - 1 + d};
-        fill_enclosed_tiles(map_of_maze, visited, neighbour, {lines.size(), lines[0].length()});
-        if (d != last_direction) {
-            neighbour = {d == NORTH || d == WEST ? i - 1 : i + 1, d == NORTH || d == EAST ? j + 1 : j - 1};
-            //neighbour = {d == NORTH || d == WEST ? i + 1 : i - 1, d == NORTH || d == EAST ? j - 1 : j + 1};
-            fill_enclosed_tiles(map_of_maze, visited, neighbour, {lines.size(), lines[0].length()});
+std::size_t get_number_of_enclosed_tiles(std::vector<std::string> lines, const auto& start_pos) {
+    std::unordered_map<std::size_t, std::vector<std::size_t>> map_of_maze{};
+    lines[start_pos.first][start_pos.second] = get_maze_map(lines, map_of_maze, start_pos, start_pos);
+    std::size_t total{0};
+    for (auto& [y, x_list] : map_of_maze) {
+        std::sort(x_list.begin(), x_list.end());
+        std::size_t last_x{x_list[0]};
+        bool inside{false};
+        for (auto& x : x_list) {
+            if (contains({'J', '|', 'L'}, lines[y][x])) {
+                if (inside)
+                    total += x - last_x - 1;
+                inside = !inside;
+                last_x = x;
+            }
+            else if (!inside) last_x = x;
+            else last_x++;
         }
     }
-    return std::count_if(visited.begin(), visited.end(), [](auto element) { return element > 0; });
+    return total;
 }
 
-std::size_t get_loop_length(const std::vector<std::string>& lines, position start_pos) {
-    pos_vec map_of_maze{};
+std::size_t get_loop_length(const std::vector<std::string>& lines, auto& start_pos) {
+    std::unordered_map<std::size_t, std::vector<std::size_t>> map_of_maze{};
     get_maze_map(lines, map_of_maze, start_pos, start_pos);
-    return map_of_maze.size();
+    std::size_t total{0};
+    for (auto& [y, x_list] : map_of_maze)
+        total += x_list.size();
+    return total;
 }
 
-position find_starting_position(const std::vector<std::string>& lines) {
+std::pair<std::size_t, std::size_t> find_starting_position(const std::vector<std::string>& lines) {
     for (std::size_t i = 0; i < lines.size(); i++)
         for (std::size_t j = 0; j < lines[0].length(); j++)
             if (lines[i][j] == 'S')
@@ -81,7 +84,7 @@ position find_starting_position(const std::vector<std::string>& lines) {
 }
 
 void solve(const std::vector<std::string>& lines) {
-    position starting_position{find_starting_position(lines)};
+    auto starting_position{find_starting_position(lines)};
     std::cout << "1) Result is " << (get_loop_length(lines, starting_position) + 1) / 2 << std::endl;
     std::cout << "2) Result is " << get_number_of_enclosed_tiles(lines, starting_position) << std::endl;
 }
