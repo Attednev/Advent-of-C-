3bb1c4811f2cee3f7b1ceb8bc4558f04660e2ec5
Damian Gasser <damian.gasser@gmail.com>
2023-12-24 16:03:31 +0100

    Update first part of the solution

--- day24.cpp
+++ day24.cpp
@@ -1,27 +1,14 @@
 #include "day0.h"
 
-struct delta {
-    double x;
-    double y;
-    double z;
-};
-
-struct point {
-    std::size_t x;
-    std::size_t y;
-    std::size_t z;
-};
-
 struct function {
     double coefficient_x;
-    double coefficient_y;
+    double constant;
 };
 
 void solve(const std::vector<std::string>& input) {
     std::vector<function> functions{};
-    std::vector<point> points{};
-    std::vector<delta> deltas{};
-    std::vector<std::string> lines{};
+    std::vector<std::size_t> points{};
+    std::vector<std::size_t> deltas{};
     for (auto& line : trim_lines(input)) {
         auto splits{split(line, '@')};
         auto point{to_number_svector(split(splits[0], ", "))};
@@ -29,8 +16,8 @@ void solve(const std::vector<std::string>& input) {
         auto slope{(double)delta[1] / (double)delta[0]};
         auto y_intercept{(double)point[1] - slope * (double)point[0]};
         functions.emplace_back(slope, y_intercept);
-        points.emplace_back(point[0], point[1]);
-        deltas.emplace_back(delta[0], delta[1]);
+        points.emplace_back(point[0]);
+        deltas.emplace_back(delta[0]);
     }
 
     std::size_t number_of_collisions{0};
@@ -42,8 +29,8 @@ void solve(const std::vector<std::string>& input) {
             auto intersection_y{a1 * intersection_x + b1};
             if (intersection_x >= 200000000000000 && intersection_x <= 400000000000000 &&
                     intersection_y >= 200000000000000 && intersection_y <= 400000000000000 &&
-                    ((deltas[i].x > 0) == (intersection_x > points[i].x) &&
-                    (deltas[j].x > 0) == (intersection_x > points[j].x))) {
+                    ((deltas[i] > 0) == (intersection_x > points[i]) &&
+                    (deltas[j] > 0) == (intersection_x > points[j]))) {
                 number_of_collisions++;
             }
         }
