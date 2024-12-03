077ea236401545c07364347cd2827c1d34ebf591
Damin Gasser <ge47woy@mytum.de>
2024-12-03 08:48:14 +0100

    Add option to get input as string instead of lines

--- 2024/helper.py
+++ 2024/helper.py
@@ -4,3 +4,10 @@ def get_input():
     lines = file.read().split('\n')
     file.close()
     return lines
+
+
+def get_input_as_str():
+    file = open('input.txt', 'r')
+    lines = file.read()
+    file.close()
+    return lines
